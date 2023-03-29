3939b3da9a0865731176a5e821b5087c8e05d081
hilski@mail.ru <hs60ssbv>
2023-03-29 21:57:50 +0300

    Share link project

---
+++ app/src/main/java/com/example/sharelink/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.sharelink
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "https://unsplash.com/photos/JreKK_t1Ycc")
+            startActivity(Intent.createChooser(intent, "Choose one"))
+        }
+    }
+}
